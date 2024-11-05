2beeafa01412ce16071a40e9c9db31247b0a11de
Yasmin Lopes Borba <yasmin.b0rbalopes@gmail.com>
2024-11-05 14:03:42 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+    $servername = "localhost:3309";
+    $username = "root";
+    $password = "";
+    $dbname = "crudS";
+
+// Váriavel de conexão (todos os parâmetros do banco)
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
